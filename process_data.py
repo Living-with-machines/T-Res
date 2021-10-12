3497ae2be7aea8dced12a6d30c18780473a0b638
mcollardanuy <mcollardanuy@turing.ac.uk>
2021-10-12 14:15:03 +0000

    process lwm data into df

--- process_data.py
+++ process_data.py
@@ -1,3 +1,51 @@
+import glob
+import pandas as pd
+from pathlib import Path
 from utils import get_data
 
+# Path for the output dataset dataframes:
+output_path = "outputs/data/"
+
+# ------------------------------------------------------
+# LWM dataset
+# ------------------------------------------------------
+
+# Download the annotated data from the BL repository:
 get_data.download_lwm_data()
+
+# Path of the annotated data:
+tsv_topres_path = "resources/topRes19th/annotated_tsv/"
+
+# Create the dataframe where we will store our annotated
+# data in a format that works better for us:
+df = pd.DataFrame(columns = ["mention_id", "sent_id", "article_id", "place", "decade", "prev_sentence", "current_sentence", "marked_sentence", "next_sentence", "mention", "place_class", "place_wikiid"])
+
+# Populate the dataframe of toponyms and annotations:
+mention_counter = 0
+for fid in glob.glob(tsv_topres_path + "*"):
+    
+    filename = fid.split("/")[-1] # Full document name
+    file_id = filename.split("_")[0] # Document id
+    publ_place = filename.split("_")[1][:-8] # Publication place
+    publ_decade = filename[-8:-4] # Publication decade
+    
+    # Dictionary that maps each token in a document with its
+    # positional information and associated annotations:
+    dTokens = get_data.process_tsv(tsv_topres_path + filename)
+    
+    # Dictionary of reconstructed sentences:
+    dSentences = get_data.reconstruct_sentences(dTokens)
+    
+    for k in dTokens:
+        # For each token, create the list that will become the dataframe
+        # row, if the token has been annotated:
+        row = get_data.create_lwmdf_row(dTokens[k], file_id, publ_place, publ_decade, mention_counter, dSentences)
+        # If a row has been created:
+        if row:
+            # Convert the row into a pd.Series:
+            row = pd.Series(row, index=df.columns)
+            # And append it to the main dataframe:
+            df = df.append(row, ignore_index=True)
+
+Path(output_path).mkdir(parents=True, exist_ok=True)
+df.to_csv(output_path + "lwm_df.tsv", sep="\t")
