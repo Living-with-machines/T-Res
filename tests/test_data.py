430cc71acf807745a93afa6235c1325c0a8434e2
Federico Nanni <nanni.federico@gmail.com>
2021-11-23 10:54:23 +0000

    Add test example

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import pytest
+import json
+from pathlib import Path
 
 def test_lwm_data_exists():
-    from pathlib import Path
     assert Path("resources/topRes19th/").is_dir()
+    assert Path("resources/wikipedia/test-extractedResources/").is_dir()
+    assert Path("resources/wikipedia/test-extractedResources/entities_overall_dict.json").is_file()
+
+def test_entity_dict():
+    with open('resources/wikipedia/test-extractedResources/entities_overall_dict.json', 'r') as f:
+        entities_overall_dict = json.load(f)
+    assert len(entities_overall_dict)==4540
