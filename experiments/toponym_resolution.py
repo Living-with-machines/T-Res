eaaffab20ba9d220e90fbab91f5f5d3b356c59b7
Federico Nanni <nanni.federico@gmail.com>
2022-03-15 09:47:27 +0000

    Add initial sketch

--- experiments/toponym_resolution.py
+++ experiments/toponym_resolution.py
@@ -11,7 +11,7 @@ import tqdm
 dataset = "lwm"
 
 # Approach:
-ner_model_id = 'lwm'
+ner_model_id = 'lwm' # or rel
 cand_select_method = 'perfectmatch' # either perfectmatch or deezymatch
 top_res_method = 'mostpopular'
 
@@ -35,36 +35,50 @@ dPreds = dict()
 dTrues = dict()
 
 for sent_id in tqdm.tqdm(dSentences.keys()):
-    # Toponym recognition
-    gold_standard, predictions = ner.ner_predict(dSentences[sent_id], dAnnotated[sent_id], ner_pipe)
-    sentence_preds = [[x["word"], x["entity"], "O"] for x in predictions]
-    sentence_trues = [[x["word"], x["entity"], x["link"]] for x in gold_standard]
 
-    pred_mentions_sent = ner.aggregate_mentions(sentence_preds)
-    true_mentions_sent = ner.aggregate_mentions(sentence_trues)
+    if ner_model_id == 'rel':
+        import requests
 
-    # Candidate selection
-    mentions = list(set([mention['mention'] for mention in pred_mentions_sent]))
-    cands = candidate_selection.select(mentions,cand_select_method)
-    
-    # # Toponym resolution
-    for mention in pred_mentions_sent:
-        text_mention = mention['mention']
-        start_offset = mention['start_offset']
-        end_offset = mention['end_offset']
+        API_URL = "https://rel.cs.ru.nl/api"
 
-        # to be extended so that it can include multiple features and can consider sentence / document context
-        res = linking.select(cands[text_mention],top_res_method)
-        if res:
-            link,score,other_cands = res
-            for x in range(start_offset,end_offset+1):
-                position_ner = sentence_preds[x][1][:2]
-                sentence_preds[x][2] = position_ner+link
-                sentence_preds[x].append(other_cands)
+        # Example EL.
+        el_result = requests.post(API_URL, json={
+            "text": dSentences[sent_id],
+            "spans": []
+        }).json()
+        print (el_result)
 
-    dPreds[sent_id] = sentence_preds
-    dTrues[sent_id] = sentence_trues
-    true_mentions_sents[sent_id] = true_mentions_sent
+    else:
+        # Toponym recognition
+        gold_standard, predictions = ner.ner_predict(dSentences[sent_id], dAnnotated[sent_id], ner_pipe)
+        sentence_preds = [[x["word"], x["entity"], "O"] for x in predictions]
+        sentence_trues = [[x["word"], x["entity"], x["link"]] for x in gold_standard]
+
+        pred_mentions_sent = ner.aggregate_mentions(sentence_preds)
+        true_mentions_sent = ner.aggregate_mentions(sentence_trues)
+
+        # Candidate selection
+        mentions = list(set([mention['mention'] for mention in pred_mentions_sent]))
+        cands = candidate_selection.select(mentions,cand_select_method)
+        
+        # # Toponym resolution
+        for mention in pred_mentions_sent:
+            text_mention = mention['mention']
+            start_offset = mention['start_offset']
+            end_offset = mention['end_offset']
+
+            # to be extended so that it can include multiple features and can consider sentence / document context
+            res = linking.select(cands[text_mention],top_res_method)
+            if res:
+                link,score,other_cands = res
+                for x in range(start_offset,end_offset+1):
+                    position_ner = sentence_preds[x][1][:2]
+                    sentence_preds[x][2] = position_ner+link
+                    sentence_preds[x].append(other_cands)
+
+        dPreds[sent_id] = sentence_preds
+        dTrues[sent_id] = sentence_trues
+        true_mentions_sents[sent_id] = true_mentions_sent
 
 process_data.store_results_hipe(dataset,ner_model_id+'+'+cand_select_method+'+'+top_res_method,  dPreds)
 process_data.store_results_hipe(dataset,'true', dTrues)
