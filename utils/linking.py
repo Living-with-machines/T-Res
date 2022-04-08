1de5be9d97b0a57961c5b96123fd195daa6888f3
mcollardanuy <mcollardanuy@turing.ac.uk>
2022-04-08 14:01:26 +0000

    Create training set for EL

--- utils/linking.py
+++ utils/linking.py
@@ -1,15 +1,25 @@
 import json
 import requests
 import operator
+import pandas as pd
+import numpy as np
 from numpy import NaN
+from ast import literal_eval
 
+# Load Wikidata mentions to QID dictionary
 wikidata_path = '/resources/wikidata/'
 with open(wikidata_path + 'mentions_to_wikidata.json', 'r') as f:
     mentions_to_wikidata = json.load(f)
 
+# Load Wikidata normalized mentions to QID dictionary
 with open(wikidata_path + 'mentions_to_wikidata_normalized.json', 'r') as f:
     normalised_mentions_to_wikidata = json.load(f)
 
+
+# -----------------------------------------
+# LINKING METHODS
+
+# Select disambiguation method
 def select(cands,method):
     if cands:
         if "mostpopular" in method:
@@ -45,3 +55,32 @@ def rel_end_to_end(sent):
         "spans": []
     }).json()
     return el_result
+
+
+def training_class_disambiguation(df):
+    
+    # Create dataframe by mention:
+    rows = []
+    for i, row in df.iterrows():
+        article_id = row["article_id"]
+        sentences = literal_eval(row["sentences"])
+        annotations = literal_eval(row["annotations"])
+        for s in sentences:
+            for a in annotations:
+                if s["sentence_pos"] == a["sent_pos"]:
+                    rows.append((article_id, s["sentence_pos"], s["sentence_text"], a["mention_pos"], a["mention"], a["wkdt_qid"], a["mention_start"], a["mention_end"]))
+
+    training_df = pd.DataFrame(columns=["article_id", "sentence_pos", "sentence", "mention_pos", "mention", "wkdt_qid", "mention_start", "mention_end"], data=rows)
+    # Drop rows with unlinked entities (DO WE WANT THIS?):
+    training_df = training_df[~training_df["wkdt_qid"].isnull()]
+
+    return training_df
+
+
+def get_candidate_wikidata_ids(cands):
+    dCands = dict()
+    for k in cands:
+        if k in mentions_to_wikidata:
+            dCands[k] = list(mentions_to_wikidata[k].keys())
+
+    return dCands
