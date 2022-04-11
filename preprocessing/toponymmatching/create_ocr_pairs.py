dd747c741fec5f6a510c2dff191c029a7c034d55
Federico Nanni <nanni.federico@gmail.com>
2022-04-11 14:17:23 +0000

    Add create datasets folder if missing + linting

--- preprocessing/toponymmatching/create_ocr_pairs.py
+++ preprocessing/toponymmatching/create_ocr_pairs.py
@@ -1,22 +1,23 @@
-import pandas as pd
 import ast
-from difflib import SequenceMatcher
 import random
+from difflib import SequenceMatcher
 from pathlib import Path
+
+import pandas as pd
 from tqdm import tqdm
 
 # Parameters:
-length_diff = 2 # accepted length difference between OCR and correction
-len_token = [5,15] # minimal and maximal length token
-chall_thr = [0.4, 0.7] # min and max threshold (in str sim) for challenging matches
-max_patience = 10000 # Break if no negative match is found after {max_patience} random comparisons
-num_chall = 10 # 1/{num_chall} is the ratio of challenging negative pairs (i.e. similar)
+length_diff = 2  # accepted length difference between OCR and correction
+len_token = [5, 15]  # minimal and maximal length token
+chall_thr = [0.4, 0.7]  # min and max threshold (in str sim) for challenging matches
+max_patience = 10000  # Break if no negative match is found after {max_patience} random comparisons
+num_chall = 10  # 1/{num_chall} is the ratio of challenging negative pairs (i.e. similar)
 
 # Dataframe with aligned trove data: # TO DO: make aligned dataset available
-df = pd.read_pickle("/resources/develop/mcollardanuy/toponym-resolution/resources/ocr/trove_subsample_aligned.pkl")
+df = pd.read_pickle("../../resources/ocr/trove_subsample_aligned.pkl")
 
 # Path where we will store the resulting toponym pairs dataset:
-output_path = "experiments/datasets/" # Path for DeezyMatch hyperparameter experiments
+output_path = "experiments/datasets/"  # Path for DeezyMatch hyperparameter experiments
 # output_path = "../../experiments/outputs/deezymatch/datasets/" # Path for final experiments
 
 Path(output_path).mkdir(parents=True, exist_ok=True)
@@ -32,31 +33,31 @@ def acceptable(a, b, threshold, len_token):
         if len_token[0] < len(b) < len_token[1]:
             if abs(len(a) - len(b)) <= 2:
                 # length difference is of 2 characters:
-                if (a == b or ((value >= threshold) and (not a in b) and (not b in a))):
+                if a == b or ((value >= threshold) and (not a in b) and (not b in a)):
                     # a is not contained in b, b is not contained in a:
                     return True
     else:
         return False
-    
-    
-# Align the OCR token with its human-corrected counterpart if they 
+
+
+# Align the OCR token with its human-corrected counterpart if they
 # are acceptable (longer than x, more similar than y, a not contained
 # in b, b not contained in a, or a equals b)
 human_to_ocr = dict()
 for i, row in df.iterrows():
-    ocrText = row['ocrText']
-    humanText = row['corrected']
-    alignments = ast.literal_eval(row['alignment'])
+    ocrText = row["ocrText"]
+    humanText = row["corrected"]
+    alignments = ast.literal_eval(row["alignment"])
     for a in alignments:
-        ocr_token = ocrText[a[0]:a[1]]
-        human_token = humanText[a[2]:a[3]]
+        ocr_token = ocrText[a[0] : a[1]]
+        human_token = humanText[a[2] : a[3]]
         if acceptable(ocr_token, human_token, chall_thr[1], len_token):
             if human_token in human_to_ocr:
                 human_to_ocr[human_token].append(ocr_token)
             else:
                 human_to_ocr[human_token] = [ocr_token]
-                
-                
+
+
 for x in human_to_ocr:
     # Artificially add s<->f confusion:
     if "s" in x[:-1]:
@@ -67,8 +68,8 @@ for x in human_to_ocr:
     randompos = random.randint(0, len(x) - 1)
     newx = x[:randompos] + randomch + x[randompos:]
     human_to_ocr[x].append(newx)
-    
-    
+
+
 # List of unique OCRed tokens in our pairs:
 all_ocr_tokens = [human_to_ocr[x] for x in human_to_ocr]
 all_ocr_tokens = [item for sublist in all_ocr_tokens for item in sublist]
@@ -87,7 +88,7 @@ for k in tqdm(human_to_ocr):
     for i in range(len(human_to_ocr[k])):
         found_negative_match = False
         # Challenging negative matches:
-        if random.randint(0,num_chall) == 0:
+        if random.randint(0, num_chall) == 0:
             counter_rand = 0
             while found_negative_match == False:
                 counter_rand += 1
@@ -97,7 +98,10 @@ for k in tqdm(human_to_ocr):
                 random_one = random.choice(all_ocr_tokens)
                 if abs(len(random_one) - len(k)) <= length_diff:
                     # Challenging match (similarity between min and max thr):
-                    if SequenceMatcher(None, random_one, k).ratio() < chall_thr[1] and SequenceMatcher(None, random_one, k).ratio() > chall_thr[0]:
+                    if (
+                        SequenceMatcher(None, random_one, k).ratio() < chall_thr[1]
+                        and SequenceMatcher(None, random_one, k).ratio() > chall_thr[0]
+                    ):
                         false_pairs.append((random_one, k, "FALSE"))
                         found_negative_match = True
                         count_false_matches += 1
@@ -110,8 +114,8 @@ for k in tqdm(human_to_ocr):
                         false_pairs.append((random_one, k, "FALSE"))
                         found_negative_match = True
                         count_false_matches += 1
-                        
-                
+
+
 random.shuffle(true_pairs)
 random.shuffle(false_pairs)
 
--- preprocessing/toponymmatching/deezy_match_experiments.py
+++ preprocessing/toponymmatching/deezy_match_experiments.py
@@ -1,23 +1,24 @@
+import json
+import time
 from difflib import SequenceMatcher
 from pathlib import Path
-from tqdm import tqdm
-import pandas as pd
-import json
-import yaml
-import time
 
-from DeezyMatch import train as dm_train
+import pandas as pd
+import yaml
+from DeezyMatch import candidate_ranker, combine_vecs
 from DeezyMatch import inference as dm_inference
-from DeezyMatch import combine_vecs
-from DeezyMatch import candidate_ranker
+from DeezyMatch import train as dm_train
+from tqdm import tqdm
 
 # Prepare output folders:
-Path('./experiments/inputs/').mkdir(parents=True, exist_ok=True)
-Path('./experiments/resources/').mkdir(parents=True, exist_ok=True)
-Path('./experiments/results/').mkdir(parents=True, exist_ok=True)
+Path("./experiments/inputs/").mkdir(parents=True, exist_ok=True)
+Path("./experiments/resources/").mkdir(parents=True, exist_ok=True)
+Path("./experiments/results/").mkdir(parents=True, exist_ok=True)
 
 # Create a good-sized list of candidates from our training set:
-train_df = pd.read_csv("../../experiments/outputs/data/lwm/linking_df_train_cands_deezymatch.tsv", sep="\t")
+train_df = pd.read_csv(
+    "../../experiments/outputs/data/lwm/linking_df_train_cands_deezymatch.tsv", sep="\t"
+)
 
 # Load Wikidata mentions-to-wqid:
 wikidata_path = "/resources/wikidata/"
@@ -35,16 +36,16 @@ for k in mentions_to_wikidata:
         else:
             wikidata_to_mentions[nk] = [k]
 
-with open('experiments/resources/wikidata_to_mentions.json', 'w') as fp:
+with open("experiments/resources/wikidata_to_mentions.json", "w") as fp:
     json.dump(wikidata_to_mentions, fp)
 
-with open('experiments/resources/mentions_to_wikidata.json', 'w') as fp:
+with open("experiments/resources/mentions_to_wikidata.json", "w") as fp:
     json.dump(mentions_to_wikidata_keys, fp)
 
-with open('experiments/resources/wikidata_to_mentions.json') as json_file:
+with open("experiments/resources/wikidata_to_mentions.json") as json_file:
     wikidata_to_mentions = json.load(json_file)
 
-with open('experiments/resources/mentions_to_wikidata.json') as json_file:
+with open("experiments/resources/mentions_to_wikidata.json") as json_file:
     mentions_to_wikidata = json.load(json_file)
 
 queries = []
@@ -55,14 +56,18 @@ for i, row in train_df.iterrows():
         # Get potential altnames for each mention:
         potential_cands = wikidata_to_mentions[row["wkdt_qid"]]
         # Filter altnames to those that are >0.7 sim from query and max length diff of 3 char:
-        candidates += [x for x in potential_cands if SequenceMatcher(None, row["mention"], x).ratio() > 0.8]
+        candidates += [
+            x for x in potential_cands if SequenceMatcher(None, row["mention"], x).ratio() > 0.8
+        ]
         queries = list(set(queries))
         candidates = list(set(candidates))
 
-with open("./experiments/datasets/queries.txt", 'w') as f:
+Path("experiments/datasets/").mkdir(parents=True, exist_ok=True)
+
+with open("./experiments/datasets/queries.txt", "w") as f:
     f.write("\n".join(map(str, queries)))
 
-with open("./experiments/datasets/candidates.txt", 'w') as f:
+with open("./experiments/datasets/candidates.txt", "w") as f:
     f.write("\n".join(map(str, candidates)))
 
 # -----------------------------------------------------
@@ -70,7 +75,13 @@ with open("./experiments/datasets/candidates.txt", 'w') as f:
 # -----------------------------------------------------
 
 list_architectures = ["gru", "lstm"]
-list_pooling_modes = ["hstates_layers_simple", "hstates_layers", "hstates_l2_distance", "average", "max"]
+list_pooling_modes = [
+    "hstates_layers_simple",
+    "hstates_layers",
+    "hstates_l2_distance",
+    "average",
+    "max",
+]
 list_dropouts = [0.01, 0.5]
 list_learning_rates = [0.01, 0.001]
 
@@ -103,7 +114,9 @@ for architecture in list_architectures:
                 dl_inputs["gru_lstm"]["att_dropout"] = [dropout, dropout]
                 dl_inputs["gru_lstm"]["leraning_rate"] = learning_rate
 
-                with open('./experiments/inputs/input_dfm_char_' + str(experiment_id) + '.yaml', 'w') as outfile:
+                with open(
+                    "./experiments/inputs/input_dfm_char_" + str(experiment_id) + ".yaml", "w"
+                ) as outfile:
                     yaml.dump(dl_inputs, outfile, explicit_start=True, indent=2)
 
                 # --------------------------------------
@@ -112,71 +125,122 @@ for architecture in list_architectures:
                 dm_model = "char_" + str(experiment_id)
 
                 # If model does not exist already, train a new model:
-                if not Path("./experiments/models/" + dm_model + '/' + dm_model + '.model').is_file():
+                if not Path(
+                    "./experiments/models/" + dm_model + "/" + dm_model + ".model"
+                ).is_file():
                     # train a new model
-                    dm_train(input_file_path="./experiments/inputs/input_dfm_char_" + str(experiment_id) + ".yaml",
+                    dm_train(
+                        input_file_path="./experiments/inputs/input_dfm_char_"
+                        + str(experiment_id)
+                        + ".yaml",
                         dataset_path="./experiments/datasets/ocr_string_pairs.txt",
-                        model_name=dm_model)
+                        model_name=dm_model,
+                    )
 
                 # --------------------------------------
                 # Generate and combine vectors for candidates:
 
-                if not Path("./experiments/candidate_vectors/candidates_" + dm_model + "/embeddings/").is_dir():
+                if not Path(
+                    "./experiments/candidate_vectors/candidates_" + dm_model + "/embeddings/"
+                ).is_dir():
                     start_time = time.time()
-                    dm_inference(input_file_path="./experiments/models/" + dm_model + "/input_dfm_char_" + str(experiment_id) + ".yaml",
-                                    dataset_path="./experiments/datasets/candidates.txt", 
-                                    pretrained_model_path="./experiments/models/" + dm_model + "/" + dm_model + ".model", 
-                                    pretrained_vocab_path="./experiments/models/" + dm_model + "/" + dm_model + ".vocab",
-                                    inference_mode="vect",
-                                    scenario="./experiments/candidate_vectors/candidates_" + dm_model)
+                    dm_inference(
+                        input_file_path="./experiments/models/"
+                        + dm_model
+                        + "/input_dfm_char_"
+                        + str(experiment_id)
+                        + ".yaml",
+                        dataset_path="./experiments/datasets/candidates.txt",
+                        pretrained_model_path="./experiments/models/"
+                        + dm_model
+                        + "/"
+                        + dm_model
+                        + ".model",
+                        pretrained_vocab_path="./experiments/models/"
+                        + dm_model
+                        + "/"
+                        + dm_model
+                        + ".vocab",
+                        inference_mode="vect",
+                        scenario="./experiments/candidate_vectors/candidates_" + dm_model,
+                    )
                     elapsed = time.time() - start_time
                     print("Generate candidate vectors: %s" % elapsed)
 
                 if not Path("./experiments/combined/candidates_" + dm_model).is_dir():
                     start_time = time.time()
-                    combine_vecs(rnn_passes=["fwd", "bwd"], 
-                                    input_scenario="./experiments/candidate_vectors/candidates_" + dm_model, 
-                                    output_scenario="./experiments/combined/candidates_" + dm_model, 
-                                    print_every=100000)
+                    combine_vecs(
+                        rnn_passes=["fwd", "bwd"],
+                        input_scenario="./experiments/candidate_vectors/candidates_" + dm_model,
+                        output_scenario="./experiments/combined/candidates_" + dm_model,
+                        print_every=100000,
+                    )
                     elapsed = time.time() - start_time
                     print("Combine candidate vectors: %s" % elapsed)
 
                 # --------------------------------------
                 # Generate and combine vectors for queries:
 
-                if not Path("./experiments/query_vectors/queries_" + dm_model + "/embeddings/").is_dir():
+                if not Path(
+                    "./experiments/query_vectors/queries_" + dm_model + "/embeddings/"
+                ).is_dir():
                     start_time = time.time()
-                    dm_inference(input_file_path="./experiments/models/" + dm_model + "/input_dfm_char_" + str(experiment_id) + ".yaml",
-                                    dataset_path="./experiments/datasets/queries.txt", 
-                                    pretrained_model_path="./experiments/models/" + dm_model + "/" + dm_model + ".model", 
-                                    pretrained_vocab_path="./experiments/models/" + dm_model + "/" + dm_model + ".vocab",
-                                    inference_mode="vect",
-                                    scenario="./experiments/query_vectors/queries_" + dm_model)
+                    dm_inference(
+                        input_file_path="./experiments/models/"
+                        + dm_model
+                        + "/input_dfm_char_"
+                        + str(experiment_id)
+                        + ".yaml",
+                        dataset_path="./experiments/datasets/queries.txt",
+                        pretrained_model_path="./experiments/models/"
+                        + dm_model
+                        + "/"
+                        + dm_model
+                        + ".model",
+                        pretrained_vocab_path="./experiments/models/"
+                        + dm_model
+                        + "/"
+                        + dm_model
+                        + ".vocab",
+                        inference_mode="vect",
+                        scenario="./experiments/query_vectors/queries_" + dm_model,
+                    )
                     elapsed = time.time() - start_time
                     print("Generate query vectors: %s" % elapsed)
 
                 if not Path("./experiments/combined/queries_" + dm_model).is_dir():
                     start_time = time.time()
-                    combine_vecs(rnn_passes=["fwd", "bwd"], 
-                                    input_scenario="./experiments/query_vectors/queries_" + dm_model, 
-                                    output_scenario="./experiments/combined/queries_" + dm_model, 
-                                    print_every=100000)
+                    combine_vecs(
+                        rnn_passes=["fwd", "bwd"],
+                        input_scenario="./experiments/query_vectors/queries_" + dm_model,
+                        output_scenario="./experiments/combined/queries_" + dm_model,
+                        print_every=100000,
+                    )
                     elapsed = time.time() - start_time
                     print("Combine query vectors: %s" % elapsed)
-                
+
                 # --------------------------------------
                 # Rank candidates
 
-                candidates_pd = \
-                    candidate_ranker(query_scenario="./experiments/combined/queries_" + dm_model,
-                                candidate_scenario="./experiments/combined/candidates_" + dm_model, 
-                                ranking_metric="faiss", 
-                                selection_threshold=20., 
-                                num_candidates=3, 
-                                search_size=3, 
-                                output_path="./experiments/ranker_results/queries_candidates_" + dm_model, 
-                                pretrained_model_path="./experiments/models/" + dm_model + "/" + dm_model + ".model", 
-                                pretrained_vocab_path="./experiments/models/" + dm_model + "/" + dm_model + ".vocab")
+                candidates_pd = candidate_ranker(
+                    query_scenario="./experiments/combined/queries_" + dm_model,
+                    candidate_scenario="./experiments/combined/candidates_" + dm_model,
+                    ranking_metric="faiss",
+                    selection_threshold=20.0,
+                    num_candidates=3,
+                    search_size=3,
+                    output_path="./experiments/ranker_results/queries_candidates_" + dm_model,
+                    pretrained_model_path="./experiments/models/"
+                    + dm_model
+                    + "/"
+                    + dm_model
+                    + ".model",
+                    pretrained_vocab_path="./experiments/models/"
+                    + dm_model
+                    + "/"
+                    + dm_model
+                    + ".vocab",
+                )
 
                 # --------------------------------------
                 # Provide accuracy
@@ -187,7 +251,11 @@ for architecture in list_architectures:
                 incorrect_distance = 0.0
                 for i, row in train_df.iterrows():
                     if row["mention"] in list(candidates_pd["query"]):
-                        returned_candidates = candidates_pd[candidates_pd["query"] == row["mention"]].iloc[0].faiss_distance
+                        returned_candidates = (
+                            candidates_pd[candidates_pd["query"] == row["mention"]]
+                            .iloc[0]
+                            .faiss_distance
+                        )
                         print(row["mention"])
                         print(returned_candidates)
                         for rc in returned_candidates:
@@ -207,7 +275,13 @@ for architecture in list_architectures:
                     fw.write("Learning rate:" + str(learning_rate) + "\n\n")
                     fw.write("Correct:" + str(len(correct)) + "\n")
                     fw.write("Incorrect:" + str(len(incorrect)) + "\n")
-                    fw.write("Accuracy:" + str(len(correct)/(len(correct)+len(incorrect))) + "\n")
+                    fw.write(
+                        "Accuracy:" + str(len(correct) / (len(correct) + len(incorrect))) + "\n"
+                    )
                     fw.write("Correct distance:" + str(correct_distance) + "\n")
                     fw.write("Incorrect distance:" + str(incorrect_distance) + "\n")
-                    fw.write("Distance accuracy:" + str(correct_distance/(correct_distance+incorrect_distance)) + "\n")
+                    fw.write(
+                        "Distance accuracy:"
+                        + str(correct_distance / (correct_distance + incorrect_distance))
+                        + "\n"
+                    )
