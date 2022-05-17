b0391cba3630b167871cd3faf98bf36f5d210a96
mcollardanuy <mcollardanuy@turing.ac.uk>
2022-05-17 15:14:29 +0000

    Add dev split

--- experiments/data_processing.py
+++ experiments/data_processing.py
@@ -7,6 +7,9 @@ from pathlib import Path
 from utils import get_data
 from utils import process_data
 from sklearn.model_selection import train_test_split
+import random
+
+random.seed(42)
 
 
 # ------------------------------------------------------
@@ -44,14 +47,22 @@ lwm_all_df = pd.concat([lwm_train_df, lwm_test_df])
 
 groups = [i for i, group in lwm_all_df.groupby(["place", "decade"])]
 for group in groups:
+    # Remove the split that will be the test set from the list of splits:
+    remainers = [other_group for other_group in groups if not other_group == group]
+    # Randomly pick one split from the remainers to be the dev set:
+    dev_group = random.choice(remainers)
+    # Name the experiment after the test split:
     group_name = group[0].split("-")[0] + str(group[1])
+    # Assign each file to train, dev, or test for each experiment:
     train_test_column = []
     for i, row in lwm_all_df.iterrows():
         if row["place"] == group[0] and row["decade"] == group[1]:
             train_test_column.append("test")
+        elif row["place"] == dev_group[0] and row["decade"] == dev_group[1]:
+            train_test_column.append("dev")
         else:
             train_test_column.append("train")
-
+    # Store the split in a column named after the experiment (after the test split):
     lwm_all_df[group_name] = train_test_column
 
 # Store dataframe:
@@ -60,6 +71,15 @@ lwm_all_df.to_csv(
     sep="\t",
     index=False,
 )
+
+print()
+print("===================")
+print("### LWM experiments")
+print("===================\n")
+for group in groups:
+    group_name = group[0].split("-")[0] + str(group[1])
+    print(lwm_all_df[group_name].value_counts())
+print()
 
 # ------------------------------------------------------
 # CLEF HIPE dataset
@@ -81,23 +101,37 @@ hipe_test_df = process_data.process_hipe_for_linking(
 
 hipe_all_df = pd.concat([hipe_dev_df, hipe_test_df])
 
-all_publications = sorted(list(hipe_all_df.publication_code.unique()))
+# Split dev set into train and dev set, by article:
+hipe_train_df, hipe_dev_df = train_test_split(
+    hipe_dev_df, test_size=0.5, random_state=42
+)
+train_ids = list(hipe_train_df.article_id.unique())
+dev_ids = list(hipe_dev_df.article_id.unique())
+test_ids = list(hipe_dev_df.article_id.unique())
 
-exp_n = 0
-for comb_group in list(process_data.split(all_publications, 3)):
-    exp_n += 1
-    train_test_column = []
-    for i, row in hipe_all_df.iterrows():
-        if row["publication_code"] in comb_group:
-            train_test_column.append("test")
-        else:
-            train_test_column.append("train")
+dev_test = []  # Original split: into dev and test only
+train_dev_test = []  # Following the original split, but dev split into train and dev
+for i, row in hipe_all_df.iterrows():
+    if row["article_id"] in train_ids:
+        dev_test.append("dev")
+        train_dev_test.append("train")
+    elif row["article_id"] in dev_ids:
+        dev_test.append("dev")
+        train_dev_test.append("dev")
+    else:
+        dev_test.append("test")
+        train_dev_test.append("test")
 
-    hipe_all_df["Exp" + str(exp_n)] = train_test_column
+# Store the split in a column named after the experiment:
+hipe_all_df["originalsplit"] = dev_test
+hipe_all_df["traindevtest"] = train_dev_test
 
 # Store dataframe:
-hipe_all_df.to_csv(
-    output_path_hipe + "linking_df_split.tsv",
-    sep="\t",
-    index=False,
-)
+hipe_all_df.to_csv(output_path_hipe + "linking_df_split.tsv", sep="\t", index=False)
+
+print("===================")
+print("### HIPE experiments")
+print("===================\n")
+for group_name in ["originalsplit", "traindevtest"]:
+    print(hipe_all_df[group_name].value_counts())
+print()
