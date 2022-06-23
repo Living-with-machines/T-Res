9a748535c29febff1e2b39926459b44cff57c074
Federico Nanni <nanni.federico@gmail.com>
2022-06-23 12:21:07 +0000

    Update path

--- utils/REL/ner/__init__.py
+++ utils/REL/ner/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from REL.REL.ner.base import NERBase, Span
-from REL.REL.ner.flair_wrapper import load_flair_ner
-from REL.REL.ner.ngram import Cmns
+from utils.REL.ner.base import NERBase, Span
+from utils.REL.ner.flair_wrapper import load_flair_ner
+from utils.REL.ner.ngram import Cmns
