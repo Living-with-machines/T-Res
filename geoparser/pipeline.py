3225e3490931ea0a18af0dcab10b84dec7a823fe
fedenanni <fnanni@turing.ac.uk>
2022-11-14 13:48:40 +0000

    Add return candidates to mostpopular pipeline

--- geoparser/pipeline.py
+++ geoparser/pipeline.py
@@ -1,12 +1,13 @@
+import json
 import os
 import sys
-import json
 from pathlib import Path
+
 from sentence_splitter import split_text_into_sentences
 
 # Add "../" to path to import utils
 sys.path.insert(0, os.path.abspath(os.path.pardir))
-from geoparser import recogniser, ranking, linking
+from geoparser import linking, ranking, recogniser
 from utils import ner, pipeline_utils
 
 
@@ -133,24 +134,19 @@ class Pipeline:
                 self.mylinker.rel_params["model"],
             ) = self.mylinker.disambiguation_setup(experiment_name)
 
-    def run_sentence(
-        self, sentence, sent_idx=0, context=("", ""), place="", place_wqid=""
-    ):
+    def run_sentence(self, sentence, sent_idx=0, context=("", ""), place="", place_wqid=""):
         sentence = sentence.replace("—", ";")
         # Get predictions:
         predictions = self.myner.ner_predict(sentence)
         # Process predictions:
         procpreds = [
-            [x["word"], x["entity"], "O", x["start"], x["end"], x["score"]]
-            for x in predictions
+            [x["word"], x["entity"], "O", x["start"], x["end"], x["score"]] for x in predictions
         ]
         # Aggretate mentions:
         mentions = ner.aggregate_mentions(procpreds, "pred")
 
         # Perform candidate ranking:
-        wk_cands, self.myranker.already_collected_cands = self.myranker.find_candidates(
-            mentions
-        )
+        wk_cands, self.myranker.already_collected_cands = self.myranker.find_candidates(mentions)
 
         # Linking settings
         if self.mylinker.method == "reldisamb":
@@ -190,11 +186,10 @@ class Pipeline:
             for i in range(len(mentions_dataset["linking"])):
                 mention = mentions_dataset["linking"][i]
                 # Run entity linking per mention:
-                selected_cand = self.mylinker.run(
-                    {"candidates": wk_cands[mention["mention"]]}
-                )
+                selected_cand = self.mylinker.run({"candidates": wk_cands[mention["mention"]]})
                 mentions_dataset["linking"][i]["prediction"] = selected_cand[0]
                 mentions_dataset["linking"][i]["ed_score"] = round(selected_cand[1], 3)
+                mentions_dataset["linking"][i]["candidates"] = selected_cand[2]
 
         # Process output:
         keys = [
@@ -206,13 +201,12 @@ class Pipeline:
             "prediction",
             "ed_score",
             "sentence",
+            "candidates",
         ]
         sentence_dataset = []
         for md in mentions_dataset["linking"]:
             md = dict((k, md[k]) for k in md if k in keys)
-            md["latlon"] = self.mylinker.linking_resources["wqid_to_coords"].get(
-                md["prediction"]
-            )
+            md["latlon"] = self.mylinker.linking_resources["wqid_to_coords"].get(md["prediction"])
             md["wkdt_class"] = self.mylinker.linking_resources["entity2class"].get(
                 md["prediction"]
             )
---
+++ tests/test_pipeline.py
@@ -0,0 +1,84 @@
+import os
+import sys
+from pathlib import Path
+
+sys.path.insert(0, os.path.abspath(os.path.pardir))
+from geoparser import linking, pipeline, ranking, recogniser
+
+
+def test_deezy_mostpopular():
+
+    myner = recogniser.Recogniser(
+        model_name="blb_lwm-ner",  # NER model name prefix (will have suffixes appended)
+        model=None,  # We'll store the NER model here
+        pipe=None,  # We'll store the NER pipeline here
+        base_model="resources/models/bert/bert_1760_1900/",  # Base model to fine-tune
+        train_dataset="experiments/outputs/data/lwm/ner_df_train.json",  # Training set (part of overall training set)
+        test_dataset="experiments/outputs/data/lwm/ner_df_dev.json",  # Test set (part of overall training set)
+        output_model_path="experiments/outputs/models/",  # Path where the NER model is or will be stored
+        training_args={
+            "learning_rate": 5e-5,
+            "batch_size": 16,
+            "num_train_epochs": 4,
+            "weight_decay": 0.01,
+        },
+        overwrite_training=False,  # Set to True if you want to overwrite model if existing
+        do_test=False,  # Set to True if you want to train on test mode
+        training_tagset="fine",  # Options are: "coarse" or "fine"
+    )
+
+    myranker = ranking.Ranker(
+        method="deezymatch",
+        resources_path="resources/wikidata/",
+        mentions_to_wikidata=dict(),
+        wikidata_to_mentions=dict(),
+        wiki_filtering={
+            "top_mentions": 3,  # Filter mentions to top N mentions
+            "minimum_relv": 0.03,  # Filter mentions with more than X relv
+        },
+        strvar_parameters={
+            # Parameters to create the string pair dataset:
+            "ocr_threshold": 60,
+            "top_threshold": 85,
+            "min_len": 5,
+            "max_len": 15,
+        },
+        deezy_parameters={
+            # Paths and filenames of DeezyMatch models and data:
+            "dm_path": str(Path("experiments/outputs/deezymatch/").resolve()),
+            "dm_cands": "wkdtalts",
+            "dm_model": "w2v_ocr",
+            "dm_output": "deezymatch_on_the_fly",
+            # Ranking measures:
+            "ranking_metric": "faiss",
+            "selection_threshold": 25,
+            "num_candidates": 3,
+            "search_size": 3,
+            "verbose": False,
+            # DeezyMatch training:
+            "overwrite_training": False,
+            "w2v_ocr_path": str(Path("experiments/outputs/models/").resolve()),
+            "w2v_ocr_model": "w2v_*_news",
+            "do_test": False,
+        },
+    )
+
+    mylinker = linking.Linker(
+        method="mostpopular",
+        resources_path="resources/wikidata/",
+        linking_resources=dict(),
+        base_model="to-be-removed",  # Base model for vector extraction
+        rel_params={},
+        overwrite_training=False,
+    )
+
+    geoparser = pipeline.Pipeline(myner=myner, myranker=myranker, mylinker=mylinker)
+
+    resolved = geoparser.run_text(
+        "A remarkable case of rattening has just occurred in the building trade at Shefrield, but also in Lancaster. Not in Nottingham though. Not in Ashton either, nor in Salop!",
+        place_wqid="Q18125",
+    )
+    assert resolved[0]['mention'] == "Shefrield"
+    assert resolved[0]['candidates']['Q665346'] == 0.006847183317771553
+    assert resolved[0]['prediction'] == 'Q42448'
+    assert resolved[0]['ed_score'] == 0.893
