6529533d4a873beb8319f1693fd0bfc035c0717e
fedenanni <fnanni@turing.ac.uk>
2023-03-23 15:29:25 +0000

    Add test for loading model from hum

--- tests/test_ner.py
+++ tests/test_ner.py
@@ -101,3 +101,28 @@ def test_ner_predict():
     preds = myner.ner_predict("- I grew up in Plymouth—Kingston.")
     assert preds[0]["word"] == "-"
     assert preds[6]["word"] == ","
+
+
+def test_ner_load_from_hub():
+
+    myner = recogniser.Recogniser(
+        model="dslim/bert-base-NER",  # Test loading from huggingface hub
+        pipe=None,  # We'll store the NER pipeline here
+        base_model="khosseini/bert_1760_1900",  # Base model to fine-tune (from huggingface)
+        train_dataset="experiments/outputs/data/lwm/ner_fine_train.json",  # Training set (part of overall training set)
+        test_dataset="experiments/outputs/data/lwm/ner_fine_dev.json",  # Test set (part of overall training set)
+        model_path="resources/models/",  # Path where the NER model is or will be stored
+        training_args={
+            "learning_rate": 5e-5,
+            "batch_size": 16,
+            "num_train_epochs": 4,
+            "weight_decay": 0.01,
+        },
+        overwrite_training=False,  # Set to True if you want to overwrite model if existing
+        do_test=False,  # Set to True if you want to train on test mode
+        load_from_hub=True,
+    )
+    model, pipe = myner.create_pipeline()
+
+    assert type(model) == str
+    assert type(pipe) == transformers.pipelines.token_classification.TokenClassificationPipeline
