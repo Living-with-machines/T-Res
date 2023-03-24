0413eb9340e96145ecb02ca33f9e55e7df36ed77
fedenanni <fnanni@turing.ac.uk>
2023-03-24 09:57:50 +0000

    Add download and creation of common_crawl.db

--- tests/test_linking.py
+++ tests/test_linking.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import sqlite3
+import numpy as np
 
 # Add "../" to path to import utils
 sys.path.insert(0, os.path.abspath(os.path.pardir))
@@ -21,6 +23,33 @@ def test_initialise_method():
     assert type(mylinker.__str__()) == str
 
 
+def test_load_resources():
+    """
+    Test initialisation works fine
+    """
+    mylinker = linking.Linker(
+        method="mostpopular",
+        resources_path="resources/",
+        linking_resources=dict(),
+        rel_params=dict(),
+        overwrite_training=False,
+    )
+    mylinker.load_resources()
+
+    conn = sqlite3.connect(mylinker.resources_path + "/rel_db/generic/common_drawl.db")
+
+    c = conn.cursor()
+
+    word = "apple"
+    c.execute("SELECT emb FROM embeddings WHERE word=?", (word,))
+    result = c.fetchone()
+
+    embedding = np.fromstring(result[0], dtype="float32")
+
+    assert type(embedding)==np.ndarray
+    
+
+
 def test_most_popular():
     mylinker = linking.Linker(
         method="mostpopular",
@@ -31,9 +60,7 @@ def test_most_popular():
     )
 
     mylinker.load_resources()
-    dict_mention = {
-        "candidates": {"London": {"Candidates": {"Q84": 0.9, "Q92561": 0.1}}}
-    }
+    dict_mention = {"candidates": {"London": {"Candidates": {"Q84": 0.9, "Q92561": 0.1}}}}
     keep_most_popular, final_score, candidates = mylinker.most_popular(dict_mention)
     assert keep_most_popular == "Q84"
     assert final_score == 0.9895689976719958
@@ -58,9 +85,7 @@ def test_by_distance():
     mylinker.load_resources()
 
     dict_mention = {
-        "candidates": {
-            "London": {"Candidates": {"Q84": 0.9, "Q92561": 0.1}, "Score": 0.397048}
-        },
+        "candidates": {"London": {"Candidates": {"Q84": 0.9, "Q92561": 0.1}, "Score": 0.397048}},
         "place_wqid": "Q84",
     }
     pred, final_score, resulting_cands = mylinker.by_distance(dict_mention)
@@ -69,9 +94,7 @@ def test_by_distance():
     assert "Q84" in resulting_cands
 
     dict_mention = {
-        "candidates": {
-            "London": {"Candidates": {"Q84": 0.9, "Q92561": 0.1}, "Score": 0.397048}
-        },
+        "candidates": {"London": {"Candidates": {"Q84": 0.9, "Q92561": 0.1}, "Score": 0.397048}},
         "place_wqid": "Q172",
     }
     pred, final_score, resulting_cands = mylinker.by_distance(dict_mention)
