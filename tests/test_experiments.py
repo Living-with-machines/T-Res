d569ca42e704651766252aede6be7b5c9dae0bab
fedenanni <fnanni@turing.ac.uk>
2023-05-02 15:44:29 +0000

    Add test to use test_split apply - currently fails

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -152,3 +152,73 @@ def test_wrong_ranker_method():
     with pytest.raises(SystemExit) as cm:
         exp.prepare_data()
     assert cm.value.code == 0
+
+
+def test_apply():
+
+    myner = recogniser.Recogniser(
+        model="blb_lwm-ner-fine",  # NER model name prefix (will have suffixes appended)
+        pipe=None,  # We'll store the NER pipeline here
+        base_model="khosseini/bert_1760_1900",  # Base model to fine-tune (from huggingface)
+        train_dataset="experiments/outputs/data/lwm/ner_fine_train.json",  # Training set (part of overall training set)
+        test_dataset="experiments/outputs/data/lwm/ner_fine_dev.json",  # Test set (part of overall training set)
+        model_path="resources/models/",  # Path where the NER model is or will be stored
+        training_args={
+            "learning_rate": 5e-5,
+            "batch_size": 16,
+            "num_train_epochs": 4,
+            "weight_decay": 0.01,
+        },
+        overwrite_training=False,  # Set to True if you want to overwrite model if existing
+        do_test=False,  # Set to True if you want to train on test mode
+        load_from_hub=False,
+    )
+
+    # Instantiate the ranker:
+    myranker = ranking.Ranker(
+        method="perfectmatch",
+        resources_path="resources/wikidata/",
+        mentions_to_wikidata=dict(),
+        wikidata_to_mentions=dict(),
+    )
+
+    # --------------------------------------
+    # Instantiate the linker:
+    mylinker = linking.Linker(
+        method="mostpopular",
+        resources_path="resources/",
+        linking_resources=dict(),
+        rel_params=dict(),
+        overwrite_training=False,
+    )
+
+    myner.train()
+    myner.pipe = myner.create_pipeline()
+
+    myranker.mentions_to_wikidata = myranker.load_resources()
+    myranker.train()
+
+    mylinker.linking_resources = mylinker.load_resources()
+
+    # --------------------------------------
+    # Instantiate the experiment:
+    exp = experiment.Experiment(
+        dataset="lwm",
+        data_path="experiments/outputs/data/",
+        dataset_df=pd.DataFrame(),
+        results_path="experiments/outputs/results/",
+        myner=myner,
+        myranker=myranker,
+        mylinker=mylinker,
+        overwrite_processing=False,  # If True, do data processing, else load existing processing, if exists.
+        processed_data=dict(),  # Dictionary where we'll keep the processed data for the experiments.
+        test_split="apply",  # "dev" while experimenting, "test" when running final experiments.
+        rel_experiments=False,  # False if we're not interested in running the different experiments with REL, True otherwise.
+    )
+
+    # Load processed data if existing:
+    exp.processed_data = exp.load_data()
+
+    exp.linking_experiments()
+
+    assert test == 2
