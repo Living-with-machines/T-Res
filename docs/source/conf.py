95dd40d0182c4892ad2ee2828b9997f4a5996c8e
Rosie Wood <rwood@turing.ac.uk>
2023-08-16 09:29:45 +0000

    update docs dependencies

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx.ext.coverage",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
